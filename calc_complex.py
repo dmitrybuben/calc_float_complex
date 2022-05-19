9f3899204d04e6b54afdb637f110686c561afc42
dmitrybuben <97685811+dmitrybuben@users.noreply.github.com>
2022-05-19 21:27:35 +0300

    initial commit

---
+++ calc_complex.py
@@ -0,0 +1,9 @@
+def calc(a:complex,b:complex,operation: str):
+    if operation == '+':
+        return a+b
+    if operation == '-':
+        return a-b
+    if operation == '*':
+        return a+b
+    if operation == '/':
+        return a/b
---
+++ calc_float.py
@@ -0,0 +1,9 @@
+def calc(a:float,b:float,operation: str):
+    if operation == '+':
+        return a+b
+    if operation == '-':
+        return a-b
+    if operation == '*':
+        return a+b
+    if operation == '/':
+        return a/b
